1cf211c10b342338f2f01f3c315ad7f45bac8344
medha <amayanegi42@gmail.com>
2022-08-26 10:01:24 +0530

    animation added

--- drumKit/index.js
+++ drumKit/index.js
@@ -1,1 +1,14 @@
-cd
+window.addEventListener('keydown',function(e){
+
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+    if(!audio)return;   //stop the functn from running all together
+    
+    audio.currentTime = 0 ;  //rewind to the start
+    audio.play();
+
+    key.classList.toggle('playing');
+    console.log(key);
+
+
+    });
